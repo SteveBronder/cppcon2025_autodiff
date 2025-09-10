e2bd0191c74f8983c191f4315390d76d6affdfe0
Steve Bronder <sbronder@flatironinstitute.org>
2025-09-10 18:11:50 -0400

    Cut a lot of the math slides

--- code/sct.cpp
+++ code/sct.cpp
@@ -18,30 +18,6 @@
 #include <benchmark/benchmark.h>
 #include <string_view>
 
-template <typename T>
-constexpr auto type_name() {
-  std::string_view name, prefix, suffix;
-#ifdef __clang__
-  name = __PRETTY_FUNCTION__;
-  prefix = "auto type_name() [T = ";
-  suffix = "]";
-#elif defined(__GNUC__)
-  name = __PRETTY_FUNCTION__;
-  prefix = "constexpr auto type_name() [with T = ";
-  suffix = "]";
-#elif defined(_MSC_VER)
-  name = __FUNCSIG__;
-  prefix = "auto __cdecl type_name<";
-  suffix = ">(void)";
-#endif
-  name.remove_prefix(prefix.size());
-  name.remove_suffix(suffix.size());
-  return name;
-}
-
-static std::pmr::monotonic_buffer_resource mbr{1<<16};
-using alloc_t = std::pmr::polymorphic_allocator<std::byte>;
-
 struct var {
     double values_;
     double adjoints_;
